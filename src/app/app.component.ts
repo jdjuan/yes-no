cf3c0ca523c23eabbb7c467d65c0c3a2371fd6eb
Santiago Suarez Quiroz <suarezquiroz@gmail.com>
2018-04-14 11:31:53 -0500

    Working decision maker, needs some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private http: HttpClient) { }
+
+  title = 'Do I know the answer?';
+  answer = 'Maybe I know ...Maybe!';
+  answerMeme = ''
+  api = 'https://yesno.wtf/api'
+  
+  apiData: any = {
+    "answer": "Maybe I know ...Maybe!",
+    "forced": true,
+    "image": "https://yesno.wtf/assets/maybe/2-adc5f184452e691c3484d4266776ed3d.gif"
+  }
+
+  ngOnInit() {
+
+    this.ask('maybe')
+
+  }
+
+  ask(forced) {
+    console.log("ask ", forced);
+    this.answer = 'let me think about it...'
+    this.http.get(this.api, { params: { 'force': forced || '' } })
+      .subscribe(data => {
+        this.apiData = data
+        this.answerMeme = data["image"];
+      });
+  }
+
+  showAnswer() {
+
+      this.answer = this.apiData["answer"];
+
+  }
 }
